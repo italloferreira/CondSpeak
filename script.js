734ecff162607b43d93f88c592053d3405ecc456
itallo ferreira <italloferreira2208@gmail.com>
2024-10-17 12:52:09 -0300

    atualização de responsividade

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-// Exemplo de script para interação básica
+
 document.querySelector('.download-btn').addEventListener('click', function () {
     alert('Download iniciado!');
+});
+
+const toggleMenu = document.querySelector('.toggle-menu');
+const header = document.querySelector('header');
+const menuOverlay = document.querySelector('.menu-overlay');
+
+toggleMenu.addEventListener('click', () => {
+    header.classList.toggle('open');
+    menuOverlay.classList.toggle('open');
+});
+
+menuOverlay.addEventListener('click', () => {
+    header.classList.remove('open');
+    menuOverlay.classList.remove('open');
 });
